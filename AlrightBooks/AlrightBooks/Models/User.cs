21d450bc5cac38fea6611b36d39c5e7a106a091c
Brian McFarlane <bmcfarlane82@gmail.com>
2017-10-10 10:04:42 -0700

    Database is created

---
+++ AlrightBooks/AlrightBooks/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlrightBooks.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string FaveGenre { get; set; }
+
+        public virtual BookList BookList { get; set; }
+        
+    }
+}
