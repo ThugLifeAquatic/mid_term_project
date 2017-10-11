062c69485481b35e93fc063dec44162d428e8362
Austin Cohrs <acohrsdev@outlook.com>
2017-10-11 16:25:06 -0700

    Added genre dropdown., and some error handling.

--- AlrightBooks/AlrightBooks/Controllers/HomeController.cs
+++ AlrightBooks/AlrightBooks/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AlrightBooks.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AlrightBooks.Controllers
 {
